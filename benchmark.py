6fcf1b3302c1797cbff0222efc3dd01aa61fad4e
Yiming Ma <yiming.ma.phd@outlook.com>
2023-03-07 20:02:48 +0000

    🦄 refactor: train and eval

--- benchmark.py
+++ benchmark.py
@@ -13,9 +13,8 @@ from tqdm import tqdm
 
 
 def benchmark(
-    model: nn.Module = timm.create_model("vit_tiny_patch16_224", pretrained=True, num_classes=10),
+    model: nn.Module = timm.create_model("vit_base_patch16_224", pretrained=True, num_classes=10),
     img_size: int = 224,
-    mode: str = "train",
     batch_size: int = 32,
     num_epochs: int = 20,
     num_workers: int = 2,
@@ -25,7 +24,6 @@ def benchmark(
 ) -> None:
     assert isinstance(model, nn.Module)
     assert isinstance(img_size, int) and img_size > 0
-    assert mode in ["train", "eval"]
     assert isinstance(batch_size, int) and batch_size > 0
     assert isinstance(num_epochs, int) and num_epochs > 0
     assert isinstance(learning_rate, float) and learning_rate > 0.0
@@ -33,37 +31,39 @@ def benchmark(
     download = not os.path.exists(os.path.join(".", "imagenette2-320"))
     if download:
         print("Data not found. Now download it.")
-    dataset = ImageNette(split=mode, size=img_size, download=download)
-    dataloader = DataLoader(
-        dataset=dataset,
+    train_dataset = ImageNette(split="train", size=img_size, download=download)
+    train_dataloader = DataLoader(
+        dataset=train_dataset,
         batch_size=batch_size,
-        shuffle=mode == "train",
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=num_workers > 0
+    )
+    val_dataset = ImageNette(split="val", size=img_size, download=download)
+    val_dataloader = DataLoader(
+        dataset=val_dataset,
+        batch_size=batch_size,
+        shuffle=False,
         num_workers=num_workers,
         pin_memory=num_workers > 0
     )
 
+    loss_fn = nn.CrossEntropyLoss(reduction="mean")
     optimizer = optim.Adam(params=model.parameters(), lr=learning_rate, weight_decay=weight_decay)
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer=optimizer, T_max=5, verbose=True)
 
     device = torch.device(device)
     model = model.to(device)
-
-    if mode == "train":
-        _train(model, dataloader, num_epochs, optimizer, scheduler, device)
-    else:
-        _eval(model, dataloader, num_epochs)
-
-
-def _train(model, dataloader, num_epochs, optimizer, scheduler, device):
-    model.train()
-
-    loss_fn = nn.CrossEntropyLoss(reduction="mean")
     loss_fn = loss_fn.to(device)
 
     for i in range(num_epochs):
-        costs, times = [], []
-        cost_, time_ = 0.0, 0.0
-        for (imgs, labels) in tqdm(dataloader):
+        costs, scores, times = [], [], []
+        cost_, y_preds, y_trues, time_ = [], [], [], 0.0
+
+        model.train()
+        print(f"Epoch: {i}")
+        print("Training")
+        for (imgs, labels) in tqdm(train_dataloader):
             imgs, labels = imgs.to(device), labels.long().to(device)
 
             tic = time()
@@ -76,23 +76,16 @@ def _train(model, dataloader, num_epochs, optimizer, scheduler, device):
             toc = time()
 
             time_ += toc - tic
-            cost_ += loss.detach().item()
+            cost_.append(loss.detach().item())
 
         scheduler.step()
-        print(f"epoch: {i}; cost: {cost_:.2f}; time: {time_:.2f}")
+        cost_ = np.mean(cost_)
+
         costs.append(cost_)
-        times.append(time_)
 
-    print(f"mean cost: {np.round(np.mean(costs), decimals=2)}; mean time: {np.round(np.mean(times), decimals=2)}")
-
-
-def _eval(model, dataloader, num_epochs, device):
-    model.eval()
-
-    for i in range(num_epochs):
-        scores, times = [], []
-        y_preds, y_trues, time_ = [], [], 0.0
-        for (imgs, labels) in tqdm(dataloader):
+        model.eval()
+        print("Evaluating")
+        for (imgs, labels) in tqdm(val_dataloader):
             imgs, labels = imgs.to(device), labels.to(device)
 
             tic = time()
@@ -110,11 +103,14 @@ def _eval(model, dataloader, num_epochs, device):
         y_preds = np.argmax(y_preds, axis=1)
         score = metrics.accuracy_score(y_pred=y_preds, y_true=y_trues)
 
-        print(f"epoch: {i}; acc: {score:.2f}; time: {time_:.2f}")
+        print(f"cost: {cost_:.2f}; acc: {score:.2f}; time: {time_:.2f}")
         scores.append(score)
         times.append(time_)
 
-    print(f"mean acc: {np.round(np.mean(scores), decimals=2)}; mean time: {np.round(np.mean(times), decimals=2)}")
+    print("Benchmarking finished.")
+    print(f"Costs: {costs}")
+    print(f"Scores: {scores}")
+    print(f"Mean time: {np.round(np.mean(times), decimals=2)}")
 
 
 if __name__ == "__main__":
